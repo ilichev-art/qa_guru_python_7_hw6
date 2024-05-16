b50cddd9ebc3c6f01dd3c2cc79271334373e9e70
Artem Ilichev <artem.ilichev@list.ru>
2024-05-16 20:08:45 +0300

    homework 6 fix

--- homework.py
+++ homework.py
@@ -8,7 +8,7 @@ def test_dark_theme_by_time():
     """
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
     current_time = time(hour=23)
-    if time(hour=22) <= current_time >= time(hour=6):
+    if current_time.hour >= 22 or current_time.hour < 6:
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -25,14 +25,15 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
     current_time = time(hour=16)
+    is_dark_theme = None
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-    if dark_theme_enabled_by_user==True:
+    if dark_theme_enabled_by_user is True:
         is_dark_theme = True
-    elif dark_theme_enabled_by_user==False:
+    elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
-    elif dark_theme_enabled_by_user==None:
+    elif dark_theme_enabled_by_user is None:
         if time(hour=22) <= current_time >= time(hour=6):
             is_dark_theme = True
         else:
